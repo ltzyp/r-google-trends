9cf20f2d15dc99995b8318d2b0750292ee45edd4
ltzyp <ltzypunov@gmail.com>
2020-10-22 21:19:13 +0300

    timeseq2

--- time-intervals.r
+++ time-intervals.r
@@ -5,27 +5,17 @@ TI_parse_time <- function(string) {
     tokens <- strsplit(string,'<~',fixed=TRUE)[[1]]
      dateTokens  <- strsplit(strsplit(tokens[1],':')[[1]],'.',fixed = TRUE)
      factorTokens<- strsplit(tokens[2],'*',fixed=TRUE)[[1]]
-     tokens <- list(start=dateTokens[1][[1]],stop=dateTokens[2][[1]],times=factorTokens[1],step=factorTokens[2])
+     tokens <- list(from=dateTokens[1][[1]],to=dateTokens[2][[1]],length=factorTokens[1],by=factorTokens[2])
      parsed <-tokens[sapply(tokens,function(x ) { length(x[nchar(x)])>0 && !is.na(x) } )]
      if ( (numTokens<- length(parsed)) <3 ) stop(sprintf("not enough agruments need 3 found %s ",numTokens) )
      if ( (numTokens<- length(parsed)) >3 ) stop(sprintf("too much agruments need 3 found %s ",numTokens) )
   # todo   if ( (numTokens<- length(parsed[2:4])) <>2 ) stop(sprintf("just 2 agrs except 1st must be defined ",numTokens) )
-     start <- do.call('ISOdate',as.list(parsed[[1]]))
-     granularity <- 'day'
-    # print(parsed)
-    if ( is.null(parsed$stop) ) {
-      length<-as.integer(parsed$times[1])
-      by<-sprintf("%s %s",parsed$step[1], granularity)
-      return( seq(start, length=length, by=by))
-    }
-     if ( is.null(parsed$times) ) {
-       by<-sprintf("%s %s",parsed$step[1], granularity)
-       stop <- do.call('ISOdate',as.list(parsed$stop))
-       return( seq(start, to=stop, by=by))
-     }
-     if ( is.null(parsed$step) ) {
-       stop <- do.call('ISOdate',as.list(parsed$stop))
-       length<-as.integer(parsed$times[1])
-       return( seq(start, to=stop, length=length))
-     }
+     granularity <- c('year','month','day','hour','min','sec')[length(parsed$from)]
+     parsed$from <- do.call('ISOdate',as.list(parsed[[1]]))
+#     parsed$length<-as.integer(parsed$length[1])
+     parsed$by<-sprintf("%s %s",parsed$by[1], granularity)
+     parsed$to <- do.call('ISOdate',as.list(parsed$to))
+     print(parsed)
+ 
+         return( do.call('seq',parsed) )
 }
