d6fbce52cbbdad216873abf1a1169b9a1bc51c05
ltzyp <ltzypunov@gmail.com>
2020-11-18 22:03:19 +0300

    plot locals history

---
+++ google-trends-graphs.r
@@ -0,0 +1,53 @@
+source('time-intervals.r')
+require('gtrendsR')
+require('reshape2')
+require('ggplot2')
+default_transformer <-  function(a,b) { (a-b)/(a+b)  }
+
+GT_LocationsHistory <- function(words,geo,timeseq,func=default_transformer) {
+    func_ <- function(a,b) {tryCatch( func(a,b) ,error=function(e) NA   ) }
+    tf <- TI_frame(timeseq) 
+    df <- data.frame(timeslot=character(),location=character(),value=numeric() )
+    attr(df,'wordList') <- words
+    for ( i in seq_along(tf[,1]) ) {
+        di <-  paste(format(tf[i,],'%Y-%m-%d'),collapse=" ")
+        gt <-  gtrends(keyword=words,geo=geo,time=di)$interest_by_region
+        fr <-  dcast(gt,'location~keyword',value.var='hits')
+        fr$value <-  func_( fr[[words[1]]], fr[[words[2]]])
+        fr$timeslot <- format(tf[i,1],"%Y%m")
+        df<-rbind(df, fr[,c('timeslot','location','value')])
+    };  
+    class(df) <- c('GT_LocationsHistory',class(df))
+    return(df)
+}
+
+GT_LocationsClusterized <-  function(df,numClusters=6) {
+  mtrx<-dcast(df,location~timeslot,value.var='value')                #todo -names  
+  mtrx$means<-apply(mtrx[,2:length(mtrx)],1,function(x){m <-  mean(x); ifelse(is.na(m),1,m)  })
+#  mtrx[is.na(mtrx$means),]$means<-1
+  mtrx$clusters <- 1                                                  #todo -where this values must be calculated
+  mtrx$clusters<-as.factor(kmeans(mtrx$means,numClusters)$cluster)
+  mtrx$rank<-0 
+  for ( lev in levels(mtrx$clusters)) {
+    i =0;
+    for (loc in mtrx[mtrx$clusters==lev,]$location) { 
+      mtrx[mtrx$location==loc,]$rank<-(i=i+1) 
+    }
+  }
+  mtrx$rank<-as.factor(mtrx$rank)
+  lc <- melt(mtrx,c("location","means","clusters","rank"))
+  class(lc) <- c('GT_LocationsClusterized',class(lc))
+  return(lc)
+}
+
+plot.GT_LocationsClusterized <- function(mtrx ) {
+     ggplot(mtrx,aes(variable,value,group=location))+
+         geom_line(aes(colour=clusters,linetype=as.factor(rank))
+     )
+}
+
+plot.GT_LocationsHistory <- function( lh,numClusters =6 ) {
+  lc <- GT_LocationsClusterized( lh, numClusters )   
+  plot(lc)
+}
+
--- time-intervals.r
+++ time-intervals.r
@@ -1,21 +1,27 @@
 TI_frame <- function(timeseq,until_now=FALSE) {
+  if(typeof(timeseq)=='character') timeseq <- TI_parse_time(timeseq)
   data.frame(starts = head(timeseq,-1),stops = tail(timeseq,-1))
+}
 
 TI_parse_time <- function(string) {
-    tokens <- strsplit(string,'<~',fixed=TRUE)[[1]]
-     dateTokens  <- strsplit(strsplit(tokens[1],':')[[1]],'.',fixed = TRUE)
-     factorTokens<- strsplit(tokens[2],'*',fixed=TRUE)[[1]]
-     tokens <- list(from=dateTokens[1][[1]],to=dateTokens[2][[1]],length=factorTokens[1],by=factorTokens[2])
-     parsed <-tokens[sapply(tokens,function(x ) { length(x[nchar(x)])>0 && !is.na(x) } )]
-     if ( (numTokens<- length(parsed)) <3 ) stop(sprintf("not enough agruments need 3 found %s ",numTokens) )
-     if ( (numTokens<- length(parsed)) >3 ) stop(sprintf("too much agruments need 3 found %s ",numTokens) )
-  # todo   if ( (numTokens<- length(parsed[2:4])) <>2 ) stop(sprintf("just 2 agrs except 1st must be defined ",numTokens) )
-     granularity <- c('year','month','day','hour','min','sec')[length(parsed$from)]
-     parsed$from <- do.call('ISOdate',as.list(parsed[[1]]))
-#     parsed$length<-as.integer(parsed$length[1])
-     parsed$by<-sprintf("%s %s",parsed$by[1], granularity)
-     parsed$to <- do.call('ISOdate',as.list(parsed$to))
-     print(parsed)
- 
-         return( do.call('seq',parsed) )
+  str_to_date <-   function(ch) {
+    num <- unlist( strsplit(ch,'.',fixed=TRUE))
+    len <- length(num)
+    full<- c(num,tail(c('1','1','0','0','0'),6-len))
+    dat <- do.call('ISOdate',as.list(full))
+    attr(dat,'granularity')<-c('year','month','day','hour','min','sec')[len]
+    return(dat)
+  }
+    tokens <- strsplit(strsplit(string,'<~',fixed=TRUE)[[1]],c(':','*'),fixed=TRUE)
+    tokens <- lapply(tokens,function(x) { c( x,rep(NA,2-length(x))) })
+    start <- str_to_date(tokens[[1]][1])
+    step <-( if (!is.na(tokens[[2]][2])) {
+         sprintf('%s %s',tokens[[2]][2],attr(start,'granularity'))
+    }  else NA)
+    params <- list(from=start,
+                   to=str_to_date(tokens[[1]][2]),
+                   length=as.integer(tokens[[2]][1]),
+                   by=step 
+    )
+   do.call('seq',params[!is.na(params)])
 }
