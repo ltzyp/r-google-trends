abbb490db2fe520c8b2f7219b61d5a91c9fab200
ltzyp <ltzypunov@gmail.com>
2021-01-31 19:10:12 +0300

    plot wit means

--- google-trends-graphs.r
+++ google-trends-graphs.r
@@ -2,51 +2,65 @@ source('time-intervals.r')
 require('gtrendsR')
 require('reshape2')
 require('ggplot2')
-default_transformer <-  function(a,b) { (a-b)/(a+b)  }
+require('directlabels')
+diff_transformer <-  function(a,b) { (a-b)/(a+b)  }
+default_transformer <-  function(a,b) { b/(a+b)  }
+
 
 GT_LocationsHistory <- function(words,geo,timeseq,func=default_transformer) {
-    func_ <- function(a,b) {tryCatch( func(a,b) ,error=function(e) NA   ) }
-    tf <- TI_frame(timeseq) 
-    df <- data.frame(timeslot=character(),location=character(),value=numeric() )
-    attr(df,'wordList') <- words
-    for ( i in seq_along(tf[,1]) ) {
-        di <-  paste(format(tf[i,],'%Y-%m-%d'),collapse=" ")
-        gt <-  gtrends(keyword=words,geo=geo,time=di)$interest_by_region
-        fr <-  dcast(gt,'location~keyword',value.var='hits')
-        fr$value <-  func_( fr[[words[1]]], fr[[words[2]]])
-        fr$timeslot <- format(tf[i,1],"%Y%m")
-        df<-rbind(df, fr[,c('timeslot','location','value')])
-    };  
-    class(df) <- c('GT_LocationsHistory',class(df))
-    return(df)
+  func_ <- function(a,b) {tryCatch( func(a,b) ,error=function(e) NA   ) }
+  tf <- TI_frame(timeseq) 
+  df <- data.frame(timeslot=character(),location=character(),value=numeric() )
+  attr(df,'wordList') <- words
+  for ( i in seq_along(tf[,1]) ) {
+    di <-  paste(format(tf[i,],'%Y-%m-%d'),collapse=" ")
+    gt <-  gtrends(keyword=words,geo=geo,time=di)$interest_by_region
+    gt$keyword <- gsub('\\+','_', gsub('\\s+','', gt$keyword))
+    fr <-  dcast(gt,'location~keyword',value.var='hits')
+    fr$value <-  func_( fr[[2]], fr[[3]])
+    fr$timeslot <- format(tf[i,1],degree.get_format(tf))
+    df<-rbind(df, fr[,c('timeslot','location','value')])
+  };  
+  class(df) <- c('GT_LocationsHistory',class(df))
+  return(df)
 }
 
-GT_LocationsClusterized <-  function(df,numClusters=6) {
+GT_LocationsClusterized <-  function(df,numClusters=5) {
   mtrx<-dcast(df,location~timeslot,value.var='value')                #todo -names  
-  mtrx$means<-apply(mtrx[,2:length(mtrx)],1,function(x){m <-  mean(x); ifelse(is.na(m),1,m)  })
-#  mtrx[is.na(mtrx$means),]$means<-1
-  mtrx$clusters <- 1                                                  #todo -where this values must be calculated
-  mtrx$clusters<-as.factor(kmeans(mtrx$means,numClusters)$cluster)
+  # mtrx$means<-apply(mtrx[,2:length(mtrx)],1,function(x){m <-  mean(x); ifelse(is.na(m),1,m)  })
+  #  mtrx[is.na(mtrx$means),]$means<-1
+#  mtrx$clusters <- 1                                                  #todo -where this values must be calculated
+#  mtrx$clusters<-as.factor(kmeans(mtrx$means,numClusters)$cluster)
+  mtrx <- na.omit(mtrx)
+  km <- kmeans(mtrx[c(2:ncol(mtrx))],numClusters,nstart=64)
+  mtrx$clusters<-as.factor(km$cluster)
   mtrx$rank<-0 
   for ( lev in levels(mtrx$clusters)) {
-    i =0;
+    i =1;
     for (loc in mtrx[mtrx$clusters==lev,]$location) { 
       mtrx[mtrx$location==loc,]$rank<-(i=i+1) 
     }
   }
+  location <- paste('cluster',rownames(km$centers))
+  rank <-1 
+  clusters <- as.integer(rownames(km$centers))
+  mtrx <- rbind(mtrx,cbind(location,km$centers,clusters,rank))
   mtrx$rank<-as.factor(mtrx$rank)
-  lc <- melt(mtrx,c("location","means","clusters","rank"))
+  lc <- melt(mtrx,c("location","clusters","rank"))
   class(lc) <- c('GT_LocationsClusterized',class(lc))
   return(lc)
 }
 
 plot.GT_LocationsClusterized <- function(mtrx ) {
-     ggplot(mtrx,aes(variable,value,Adjusted,group=location,colour=clusters)) +
-       geom_line(aes(colour=clusters,linetype=as.factor(rank)))  +
-        theme(legend.position='none') +
-        labs(title='UA',x='year',y='percent' )+
-        scale_color_discrete(guide='none')+             
-        geom_dl(aes(label=substr(location,1,3)),method=list(dl.combine('first.bumpup','last.bumpup'),cex=0.7) )     
+  ggplot(mtrx,aes(variable,value,Adjusted,group=location,colour=clusters)) +
+    scale_linetype_manual(values=c(1,c(2:6),c(2:6))) + 
+    scale_size_manual(values=c(2,rep(.3,5),rep(.4,5))) + 
+    scale_alpha_manual(values=c(.2,rep(1,5),rep(.7,5))) +
+    geom_line(aes(colour=clusters,linetype=as.factor(rank),alpha=as.factor(rank),size=as.factor(rank) ))  +
+    theme(legend.position='none') +
+    labs(title='UA',x='year',y='percent' )+
+    scale_color_discrete(guide='none')+             
+    geom_dl(aes(label=paste(clusters,rank)),method=list(dl.combine('first.bumpup','last.bumpup'),cex=0.7) )     
 }
 
 plot.GT_LocationsHistory <- function( lh,numClusters =6 ) {
--- time-intervals.r
+++ time-intervals.r
@@ -1,27 +1,56 @@
+#todo syntax Y~[Y]&[seq]
+#todo add granularity attribute to output
 TI_frame <- function(timeseq,until_now=FALSE) {
   if(typeof(timeseq)=='character') timeseq <- TI_parse_time(timeseq)
-  data.frame(starts = head(timeseq,-1),stops = tail(timeseq,-1))
+  df <- data.frame(starts = head(timeseq,-1),stops = tail(timeseq,-1))
+  return( degree.from(df,timeseq) )
 }
+
 
 TI_parse_time <- function(string) {
   str_to_date <-   function(ch) {
     num <- unlist( strsplit(ch,'.',fixed=TRUE))
     len <- length(num)
     full<- c(num,tail(c('1','1','0','0','0'),6-len))
-    dat <- do.call('ISOdate',as.list(full))
-    attr(dat,'granularity')<-c('year','month','day','hour','min','sec')[len]
-    return(dat)
+    degree.set( do.call('ISOdate',as.list(full)),len )
   }
     tokens <- strsplit(strsplit(string,'<~',fixed=TRUE)[[1]],c(':','*'),fixed=TRUE)
     tokens <- lapply(tokens,function(x) { c( x,rep(NA,2-length(x))) })
     start <- str_to_date(tokens[[1]][1])
+    stop <- str_to_date(tokens[[1]][2])
+    ab <-  attr(start,'granularity')
     step <-( if (!is.na(tokens[[2]][2])) {
-         sprintf('%s %s',tokens[[2]][2],attr(start,'granularity'))
+         sprintf('%s %s',tokens[[2]][2],degree.name(start))
     }  else NA)
     params <- list(from=start,
-                   to=str_to_date(tokens[[1]][2]),
+                   to=stop,
                    length=as.integer(tokens[[2]][1]),
                    by=step 
     )
-   do.call('seq',params[!is.na(params)])
+   degree.from( do.call('seq',params[!is.na(params)]), start)
 }
+
+degrees<-c('Year','month','day','Hour','Min','Sec')
+degree <- function( obj ) {
+  return(attr(obj ,'degree' ) )
+}
+
+degree.get_format <- function( obj ) {
+  a = attr(obj,'degree')
+  l <- degrees[1:match(degree(obj),degrees)]
+  paste('%',substr(l,1,1),sep='',collapse='')
+}
+
+degree.set <- function(obj,pos) {
+  attr(obj,'degree')<-degrees[pos]
+  return(obj)
+}
+
+degree.name <- function(obj) {
+  tolower(attr(obj,'degree'))
+}
+
+degree.from <- function(dst,src) {
+  attr(dst,'degree')<-attr(src,'degree')
+  return(dst)
+  }
